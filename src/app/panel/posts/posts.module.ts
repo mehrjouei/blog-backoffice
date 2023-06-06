6bea7013b0a231bf69f2606c0fc243cac03a1b31
vahid mehrjooei <mehrjooei.v@gmail.com>
2023-06-06 22:10:22 +0200

    Add text editor library

--- src/app/panel/posts/posts.module.ts
+++ src/app/panel/posts/posts.module.ts
@@ -3,23 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { CreateEditComponent } from './create-edit/create-edit.component';
+import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 
 @NgModule({
   declarations: [PostsComponent, CreateEditComponent],
   imports: [
     CommonModule,
     PostsRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     MatInputModule,
     MatButtonModule,
     MatProgressSpinnerModule,
     MatTableModule,
+    EditorModule,
+  ],
+  providers: [
+    { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' },
   ],
 })
 export class PostsModule {}
